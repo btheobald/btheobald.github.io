5873655afd123dceba12282adc82dfa5bf9a092f
Byron Theobald <byrontheobald@gmail.com>
2018-04-01 14:36:37 +0100

    Initial new site commit

---
+++ images/script.sh
@@ -0,0 +1,18 @@
+rm newgallery.html
+rm large/*
+rm thumb/*
+cd orig-nc
+for file in *.JPG; do
+  cp -f $file "..\thumb\thumb-$file";
+  cp -f $file "..\large\large-$file";
+  cd ../
+  
+  echo '<a href="images\large\large-'$file'" title="">' >> newgallery.html;
+  echo ' <img src="images\thumb\thumb-'$file'" alt=""> </a>' >> newgallery.html;
+  
+  cd orig-nc
+done;
+
+cd ../
+mogrify -resize 300x225 -quality 80 -sharpen 3 thumb/*;
+mogrify -resize 50% -quality 40 large/*;
