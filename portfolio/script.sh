1662b7eee269c407cb386397db2a72c34d507ec0
unknown <bt4g16@soton.ac.uk>
2019-03-11 22:39:30 +0000

    Updated Photo portfolio, added page for engineering portfolio

---
+++ portfolio/script.sh
@@ -0,0 +1,18 @@
+rm newport.html
+rm large/*
+rm thumb/*
+cd orig-nc
+for file in *.JPG; do
+  cp -f $file "..\thumb\thumb-$file";
+  cp -f $file "..\large\large-$file";
+  cd ../
+  
+  echo '<a href="images\large\large-'$file'" title="">' >> newport.html;
+  echo ' <img src="images\thumb\thumb-'$file'" alt=""></a>' >> newport.html;
+  
+  cd orig-nc
+done;
+
+cd ../
+mogrify -resize 300x225 -quality 80 -sharpen 2 thumb/*;
+mogrify -resize 50% -quality 65 large/*;
