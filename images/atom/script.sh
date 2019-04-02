8169b32fbd4424464b722745c411a098aa3a349f
unknown <bt4g16@soton.ac.uk>
2019-04-02 16:24:02 +0100

    simplified project gallery script and added proj london photo

--- images/atom/script.sh
+++ images/atom/script.sh
@@ -1,20 +1,16 @@
 rm newgallery.html
 rm large/*
-rm thumb/*
 cd orig-nc
 for file in *.JPG; do
   rname=$RANDOM;
-  cp -v -f $file '..\thumb\'$rname'.jpg';
-  cp -v -f $file '..\large\'$rname'.jpg';
+  cp -v -f $file '..\large\large-'$file'';
 done;
 
-cd ../thumb
+cd ../large
 
-for file in *.jpg; do
-  echo '<a href="images\large\'$file'" title="">' >> ../newgallery.html;
-  echo ' <img src="images\thumb\'$file'" alt=""></img></a>' >> ../newgallery.html;
+for file in *.JPG; do
+  echo '<a href="images\atom\large\'$file'" title=""></a>' >> ../newgallery.html;
 done;
 
 cd ../
-mogrify -monitor -resize "300^^x225" -gravity center -crop 300x225+0+0 +repage -quality 80 -sharpen 2 thumb/*;
 mogrify -monitor -resize 50% -quality 65 large/*;
